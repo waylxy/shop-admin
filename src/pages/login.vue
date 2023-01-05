20b06d0d22b2032b9d959f43d4030a40e45d005a
waylxy <2291996552@qq.com>
2023-01-05 11:43:21 +0800

    结合apply实现样式抽离

--- src/pages/login.vue
+++ src/pages/login.vue
@@ -1,23 +1,17 @@
 <template>
-  <el-row class="bg-indigo-500 min-h-screen">
-    <el-col :lg="16" :md="12" class="flex items-center justify-center">
+  <el-row class="login-container">
+    <el-col :lg="16" :md="12" class="left">
       <div>
-        <div class="text-light-50 font-bold text-3xl mb-4">欢迎光临</div>
-        <div class="text-gray-200 text-sm">《vue3+vite》实战项目演示网站</div>
+        <div>欢迎光临</div>
+        <div>《vue3+vite》实战项目演示网站</div>
       </div>
     </el-col>
-    <el-col
-      :lg="8"
-      :md="12"
-      class="bg-light-50 flex items-center justify-center flex-col"
-    >
-      <h2 class="font-bold text-3xl text-gray-800">欢迎回来</h2>
-      <div
-        class="my-5 text-gray-300 flex items-center justify-center space-x-2"
-      >
-        <span class="h-[1px] w-16 bg-gray-200"></span>
+    <el-col :lg="8" :md="12" class="right">
+      <h2 class="title">欢迎回来</h2>
+      <div class="last">
+        <span class="line"></span>
         <span>账号密码登录</span>
-        <span class="h-[1px] w-16 bg-gray-200"></span>
+        <span class="line"></span>
       </div>
       <el-form :model="form" class="w-[250px]">
         <el-form-item>
@@ -28,10 +22,10 @@
           </el-input>
         </el-form-item>
         <el-form-item>
-          <el-input v-model="form.password" placeholder="请输入密码" >
-          <template #prefix>
-            <el-icon><Lock /></el-icon>
-          </template>
+          <el-input v-model="form.password" placeholder="请输入密码">
+            <template #prefix>
+              <el-icon><Lock /></el-icon>
+            </template>
           </el-input>
         </el-form-item>
         <el-form-item>
@@ -60,3 +54,30 @@ const onSubmit = () => {
   console.log(form);
 };
 </script>
+<style scoped>
+.login-container {
+  @apply bg-indigo-500 min-h-screen;
+  .left,
+  .right {
+    @apply flex items-center justify-center;
+  }
+  .right {
+    @apply bg-light-50  flex-col;
+    .title {
+      @apply font-bold text-3xl text-gray-800;
+    }
+    .last {
+      @apply my-5 text-gray-300 flex items-center justify-center space-x-2;
+    }
+    .line {
+      @apply h-[1px] w-16 bg-gray-200;
+    }
+  }
+  .left > div > div:first-child {
+    @apply text-light-50 font-bold text-3xl mb-4;
+  }
+  .left > div > div:last-child {
+    @apply text-gray-200 text-sm;
+  }
+}
+</style>
